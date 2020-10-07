1641ef632e0556b0f319a7fae9988a4d1edbfe4f
rotes328 <72516445+rotes328@users.noreply.github.com>
2020-10-07 17:02:35 -0400

    Update main.py
    Added error detection and CLI arguments.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import time
 from datetime import datetime
 from datetime import date
+import sys
 
 class Scraper:
 
@@ -45,23 +46,47 @@ class ProcessData:
         self.locations_list = locations_list
         for i in range (attempts):
             Scraper(self.site).scrape(self.output_file, self.locations_list)
-            time.sleep(interval)
+            if i < (attempts-1):
+                time.sleep(interval)
 
 
 loclist = ["Bakers Basin", "Oakland", "Bayonne", "Paterson", "Camden", "Rahway", "Cardiff", "Randolph", "Delanco", "Rio Grande", "Eatontown", "Salem", "Edison", "S. Plainfield", "Flemington", "Toms River", "Freehold", "Vineland", "Lodi",  "Wayne", "Newark", "W. Deptford", "N. Bergen"]
 today = date.today()
+timenow = datetime.now()
 d1 = today.strftime("%b-%d-%Y")
-output = "outputfile" + d1 + ".csv"
+d2 = timenow.strftime("%H%M")
+output = "outputfile" + d1 + "-" + d2 + ".csv"
 website = "https://www.state.nj.us/mvc/locations/agency.htm"
 
-interval_in_second = 1
-attempts = 5
+#Defaults:
+interval_in_seconds = 1
+attempts = 3
 
-print("DMV Scraper Version 2 by Mike Rotella 10-07-2020")
-print(loclist)
+def intro(a, b):
+    print("DMV Scraper Version 2 by Mike Rotella 10-07-2020")
+    print("Proccessing every {} seconds for {} attempts.".format(a, b))
+    return
+
+try:
+    if __name__ == "__main__":
+        a = int(sys.argv[1])
+        b = int(sys.argv[2])
+except:
+    a = interval_in_seconds
+    b = attempts
 
 with open(output, "a") as f:
     f.write(json.dumps(loclist))
     f.write('\n')
 
-ProcessData(website).process(interval_in_second, attempts, output, loclist)
+if len(sys.argv) == 3 and a > 0 and a < 61 and b > 0 and b < 481:
+    intro(a, b)
+    print(loclist)
+    ProcessData(website).process(a, b, output, loclist)
+else:
+    a = interval_in_seconds
+    b = attempts
+    intro(a, b)
+    print("\nUse arguments [inteval in seconds between 1 and 60] [attempts between 1 and 480] to change defaults.\n")
+    print(loclist)
+    ProcessData(website).process(a, b, output, loclist)
